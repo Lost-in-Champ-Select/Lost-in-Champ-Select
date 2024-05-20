0956f5fa8e8c8338c25059383b1abb68d9f52bed
Cory Zauss <coryclassic1989@gmail.com>
2024-05-20 14:45:06 -0400

    fix puuid obj

--- Server/database/puuidFallback.js
+++ Server/database/puuidFallback.js
@@ -1,8 +1,8 @@
 
 
-puuuidObj = {
+let puuidObj = {
   '2H0QnLfmiPxeRr7dg9PRiiBpKA086TloQenQzqHygSvVI6mOMc0haAI2o0mqy0qOMheAWXP4zv0J9w': false,
-
+  'wigtPOCYecfAzFSxjPB0ztTOssZhDK-dx1GrrrqnDQgWhR5IpzY4hBKornwPeUmcqR9P81QMK9X_ig': false
 }
 
-export default puuuidObj
+export default puuidObj
