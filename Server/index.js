aec94a5d6d9fbae80145fcf67debad8060c4d21d
Cory Zauss <coryclassic1989@gmail.com>
2024-08-13 22:10:38 -0400

    added cors to server

--- Server/index.js
+++ Server/index.js
@@ -3,9 +3,15 @@ const app = express()
 const port = 4000
 import routes from './routes/routes.js'
 import dotenv from 'dotenv'
+const cors = require("cors");
 
 dotenv.config()
 
+app.use(
+  cors({
+    origin: "https://lostinchampselect.com/",
+  })
+);
 app.use(express.json());
 app.use(express.static("build"));
 app.use('/', routes)
