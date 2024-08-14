6b9ccc6d712009d85f1c7ef6ed9cd325ca84a5e9
Cory Zauss <coryclassic1989@gmail.com>
2024-08-13 22:12:06 -0400

    update cors

--- Server/index.js
+++ Server/index.js
@@ -7,11 +7,7 @@ const cors = require("cors");
 
 dotenv.config()
 
-app.use(
-  cors({
-    origin: "https://lostinchampselect.com/",
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(express.static("build"));
 app.use('/', routes)
