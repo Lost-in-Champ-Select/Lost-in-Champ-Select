e7de86dcee068f1863fcc10e279b004e7f5a8023
Cory Zauss <coryclassic1989@gmail.com>
2024-08-19 19:03:14 -0400

    cors origins

--- Server/index.js
+++ Server/index.js
@@ -7,11 +7,30 @@ import cors from "cors";
 
 dotenv.config()
 
+const allowedOrigins = [
+  "https://lostinchampselect.com",
+  "http://localhost:9000",
+];
+
 app.use(
   cors({
-    origin: ["https://lostinchampselect.com", "http://localhost:9000"],
+    origin: function (origin, callback) {
+      // Allow requests with no origin,
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
   })
 );
+// app.use(
+//   cors({
+//     origin: "https://lostinchampselect.com",
+//   })
+// );
 app.use(express.json());
 app.use(express.static("build"));
 app.use('/', routes)
