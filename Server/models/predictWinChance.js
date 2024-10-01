8ad7949474efa1c705025a2f00ef78869fb231a5
Cory Zauss <coryclassic1989@gmail.com>
2024-10-01 00:02:41 -0400

    update variabale names in calc win rates

--- Server/models/predictWinChance.js
+++ Server/models/predictWinChance.js
@@ -4,34 +4,34 @@ export const calculateModifiedWinRates = async (champions) => {
   console.log('INCOMING CHAMPS',champions)
   // Calculate baseline values
   const averageKDA =
-    champions.reduce((sum, champ) => sum + champ.avg_kda, 0) / champions.length;
+    champions.reduce((sum, champ) => sum + champ.kda, 0) / champions.length;
   const averageGold =
-    champions.reduce((sum, champ) => sum + champ.avg_gold, 0) /
+    champions.reduce((sum, champ) => sum + champ.gold, 0) /
     champions.length;
   const averageDamage =
-    champions.reduce((sum, champ) => sum + champ.avg_damage, 0) /
+    champions.reduce((sum, champ) => sum + champ.damage, 0) /
     champions.length;
 
   // Modify original objects
   champions.forEach((champ) => {
-    let modifiedWinRate = champ.avg_win_rate;
+    let modifiedWinRate = champ.win_rate;
 
     // Modify win rate based on KDA
-    if (champ.avg_kda > averageKDA) {
+    if (champ.kda > averageKDA) {
       modifiedWinRate += 0.05; // Increase win rate by 5%
     } else {
       modifiedWinRate -= 0.05; // Decrease win rate by 5%
     }
 
     // Modify win rate based on gold earned
-    if (champ.avg_gold > averageGold) {
+    if (champ.gold > averageGold) {
       modifiedWinRate += 0.03; // Increase win rate by 3%
     } else {
       modifiedWinRate -= 0.03; // Decrease win rate by 3%
     }
 
     // Modify win rate based on damage dealt
-    if (champ.avg_damage > averageDamage) {
+    if (champ.damage > averageDamage) {
       modifiedWinRate += 0.02; // Increase win rate by 2%
     } else {
       modifiedWinRate -= 0.02; // Decrease win rate by 2%
