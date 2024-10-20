48b911543ea9707aae71d151697c5cdf7a99ca46
Cory Zauss <coryclassic1989@gmail.com>
2024-10-20 15:13:32 -0400

    logs

--- Server/models/ingest/storeMatchV3.js
+++ Server/models/ingest/storeMatchV3.js
@@ -27,6 +27,7 @@ const getEachMatchesData = async (queueType) => {
     "2H0QnLfmiPxeRr7dg9PRiiBpKA086TloQenQzqHygSvVI6mOMc0haAI2o0mqy0qOMheAWXP4zv0J9w";
 
   const getMatchIdHistoryAndStore = async (playerId, numMatches) => {
+    console.log('GETTING MATCH ID HISTORY AND STORE FOR :', playerId)
     let playerHistory = await getLastNumMatches(playerId, numMatches, queueType);
     if (!Array.isArray(playerHistory)) {
       console.log("getMatchIdHistoryAndStore ERROR");
